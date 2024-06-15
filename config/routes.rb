9009c38bebba79750f5088f7f4207f5accdc7f77
Sakuntha <sakunthadayadara2001@gmail.com>
2024-06-15 12:06:14 +0530

    Models and Migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :sensor_thresholds
+  resources :sensor_data
+  resources :sensors
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
